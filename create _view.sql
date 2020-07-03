8c03c8ca225471fd367656cef131b5730baa4dd6
SetHeader <62835330+SetHeader@users.noreply.github.com>
2020-07-03 19:19:14 +0800

    Delete create _view.sql

--- create _view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW messages_desc AS
-  SELECT * FROM messages_table ORDER BY M_TIME DESC;
-
-CREATE OR REPLACE VIEW messages_asc AS
-  SELECT * FROM messages_table ORDER BY M_TIME ASC;
-
-
